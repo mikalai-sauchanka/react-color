ce1c625e24ecd44a0fa16683affaaa41117287d3
Nick <mikalai.sauchanka@gmail.com>
2019-12-04 17:35:08 +0100

    Sketch

--- src/components/sketch/SketchFields.js
+++ src/components/sketch/SketchFields.js
@@ -1,54 +1,15 @@
 /* eslint-disable no-param-reassign */
 
-import React from 'react'
+import React, {Component} from 'react'
 import reactCSS from 'reactcss'
 import color from '../../helpers/color'
 
 import { EditableInput } from '../common'
 
-export const SketchFields = ({ onChange, rgb, hsl, hex, disableAlpha }) => {
-  const styles = reactCSS({
-    'default': {
-      fields: {
-        display: 'flex',
-        paddingTop: '4px',
-      },
-      single: {
-        flex: '1',
-        paddingLeft: '6px',
-      },
-      alpha: {
-        flex: '1',
-        paddingLeft: '6px',
-      },
-      double: {
-        flex: '2',
-      },
-      input: {
-        width: '80%',
-        padding: '4px 10% 3px',
-        border: 'none',
-        boxShadow: 'inset 0 0 0 1px #ccc',
-        fontSize: '11px',
-      },
-      label: {
-        display: 'block',
-        textAlign: 'center',
-        fontSize: '11px',
-        color: '#222',
-        paddingTop: '3px',
-        paddingBottom: '4px',
-        textTransform: 'capitalize',
-      },
-    },
-    'disableAlpha': {
-      alpha: {
-        display: 'none',
-      },
-    },
-  }, { disableAlpha })
+export class SketchFields extends Component {
+  handleChange = (data, e) => {
+    let {onChange, rgb, hsl} = this.props;
 
-  const handleChange = (data, e) => {
     if (data.hex) {
       color.isValidHex(data.hex) && onChange({
         hex: data.hex,
@@ -78,60 +39,104 @@ export const SketchFields = ({ onChange, rgb, hsl, hex, disableAlpha }) => {
         source: 'rgb',
       }, e)
     }
-  }
+  };
 
-  return (
-    <div style={ styles.fields } className="flexbox-fix">
-      <div style={ styles.double }>
-        <EditableInput
-          style={{ input: styles.input, label: styles.label }}
-          label="hex"
-          value={ hex.replace('#', '') }
-          onChange={ handleChange }
-        />
-      </div>
-      <div style={ styles.single }>
-        <EditableInput
-          style={{ input: styles.input, label: styles.label }}
-          label="r"
-          value={ rgb.r }
-          onChange={ handleChange }
-          dragLabel="true"
-          dragMax="255"
-        />
-      </div>
-      <div style={ styles.single }>
-        <EditableInput
-          style={{ input: styles.input, label: styles.label }}
-          label="g"
-          value={ rgb.g }
-          onChange={ handleChange }
-          dragLabel="true"
-          dragMax="255"
-        />
-      </div>
-      <div style={ styles.single }>
-        <EditableInput
-          style={{ input: styles.input, label: styles.label }}
-          label="b"
-          value={ rgb.b }
-          onChange={ handleChange }
-          dragLabel="true"
-          dragMax="255"
-        />
-      </div>
-      <div style={ styles.alpha }>
-        <EditableInput
-          style={{ input: styles.input, label: styles.label }}
-          label="a"
-          value={ Math.round(rgb.a * 100) }
-          onChange={ handleChange }
-          dragLabel="true"
-          dragMax="100"
-        />
-      </div>
-    </div>
-  )
+  render() {
+    let {rgb, hex, disableAlpha} = this.props;
+    const styles = reactCSS({
+      'default': {
+        fields: {
+          display: 'flex',
+          paddingTop: '4px',
+        },
+        single: {
+          flex: '1',
+          paddingLeft: '6px',
+        },
+        alpha: {
+          flex: '1',
+          paddingLeft: '6px',
+        },
+        double: {
+          flex: '2',
+        },
+        input: {
+          width: '80%',
+          padding: '4px 10% 3px',
+          border: 'none',
+          boxShadow: 'inset 0 0 0 1px #ccc',
+          fontSize: '11px',
+        },
+        label: {
+          display: 'block',
+          textAlign: 'center',
+          fontSize: '11px',
+          color: '#222',
+          paddingTop: '3px',
+          paddingBottom: '4px',
+          textTransform: 'capitalize',
+        },
+      },
+      'disableAlpha': {
+        alpha: {
+          display: 'none',
+        },
+      },
+    }, {disableAlpha})
+
+    return (
+        <div style={styles.fields} className="flexbox-fix">
+          <div style={styles.double}>
+            <EditableInput
+                style={{input: styles.input, label: styles.label}}
+                label="hex"
+                value={hex.replace('#', '')}
+                onChange={this.handleChange}
+            />
+          </div>
+          <div style={styles.single}>
+            <EditableInput
+                style={{input: styles.input, label: styles.label}}
+                label="r"
+                value={rgb.r}
+                onChange={this.handleChange}
+                dragLabel="true"
+                dragMax="255"
+            />
+          </div>
+          <div style={styles.single}>
+            <EditableInput
+                style={{input: styles.input, label: styles.label}}
+                label="g"
+                value={rgb.g}
+                onChange={this.handleChange}
+                dragLabel="true"
+                dragMax="255"
+            />
+          </div>
+          <div style={styles.single}>
+            <EditableInput
+                style={{input: styles.input, label: styles.label}}
+                label="b"
+                value={rgb.b}
+                onChange={this.handleChange}
+                dragLabel="true"
+                dragMax="255"
+            />
+          </div>
+          <div style={styles.alpha}>
+            <EditableInput
+                style={{input: styles.input, label: styles.label}}
+                label="a"
+                value={Math.round(rgb.a * 100)}
+                onChange={this.handleChange}
+                dragLabel="true"
+                dragMax="100"
+            />
+          </div>
+        </div>
+    )
+  }
 }
 
 export default SketchFields
